a1f9f3c74657e4ea0f19b0ba29c75c812614e3af
CodeNinjasCoquitlam <coquitlamninja@gmail.com>
2025-06-09 17:01:27 -0700

    June 9/25
    - added photos to the order
    - fixed camera

--- Assets/Scripts/orders.cs
+++ Assets/Scripts/orders.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class orders : MonoBehaviour
 {
@@ -33,6 +34,8 @@ public class orders : MonoBehaviour
     public int randomNum;
     public bool hasFood = false;
     public int price;
+    public Image currentOrderPhoto;
+    public Sprite[] orderPhotos;
 
     public AudioSource PuduCameBack;
 
@@ -126,7 +129,7 @@ public class orders : MonoBehaviour
     {
         startPos = transform.position;
         frenchtoast = GameObject.Find("Receivers/Pudu/French Toast");
-        
+        genOrders();
 
         //string randomOrder = GetRandomOrder();
         //Debug.Log("Order:" + randomOrder);
@@ -160,88 +163,7 @@ public class orders : MonoBehaviour
             {
                 
                 moving = false;
-                randomNum = Random.Range(1, 20);
-
-                if (randomNum == 1){
-                    order COOKIE = new order("Cookie", false, false, true, false, false, false); 
-                    COOKIE.DisplayName();
-                    price = 5;
-                } else if (randomNum == 2) {
-                    order COOKIE_LEMONADE = new order("Cookie Lemonade", false, true, true, false, false, false);
-                    COOKIE_LEMONADE.DisplayName();
-                    price = 5;
-                } else if (randomNum == 3) {
-                    order BREAD = new order("Bread", false, false, false, true, false, false);
-                    BREAD.DisplayName();
-                    price = 5;
-                } else if (randomNum == 4) {
-                    order BREAD_LEMONADE = new order("Bread Lemonade", false, false, true, true, false, false);
-                    BREAD_LEMONADE.DisplayName();
-                    price = 5;
-                } else if (randomNum == 5) {
-                    order BREAD_HAM = new order("Bread and Ham", false, false, false, true, true, false);
-                    BREAD_HAM.DisplayName();
-                    price = 5;
-                } else if (randomNum == 6) {
-                    order BREAD_HAM_LEMONADE = new order("Bread and Ham Lemonade", false, true, false, true, true, false);
-                    BREAD_HAM_LEMONADE.DisplayName();
-                } else if (randomNum == 7) {
-                    order BREAD_OLIVE = new order("Bread and Olive", false, false, false, true, false, true);
-                    BREAD_OLIVE.DisplayName();
-                    price = 5;
-                } else if (randomNum == 8) {
-                    order BREAD_HAM_OLIVE = new order("Bread and Ham and Olive", false, false, false, true, true, true);
-                    BREAD_HAM_OLIVE.DisplayName();
-                    price = 5;
-                } else if (randomNum == 9) {
-                    order BREAD_HAM_OLIVE_LEMONADE = new order("Bread and Ham and Olive Lemonade", false, true, false, true, true, true);
-                    BREAD_HAM_OLIVE_LEMONADE.DisplayName();
-                    price = 5;
-                } else if (randomNum == 10) {
-                    order EGG = new order("Egg", true, false, false, false, false, false);
-                    EGG.DisplayName();
-                    price = 5;
-                } else if (randomNum == 11) {
-                    order EGG_LEMONADE = new order("Egg Lemonade", true, true, false, false, false, false);
-                    EGG_LEMONADE.DisplayName();
-                    price = 5;
-                } else if (randomNum == 12) {
-                    order EGG_OLIVE = new order("Egg and Olive", true, false, false, false, false, true);
-                    EGG_OLIVE.DisplayName();
-                    price = 5;
-                } else if (randomNum == 13) {
-                    order EGG_OLIVE_LEMONADE = new order("Egg and Olive Lemonade", true, true, false, false, false, true);
-                    EGG_OLIVE_LEMONADE.DisplayName();
-                    price = 5;
-                } else if (randomNum == 14) {
-                    order EGG_SANDWICH = new order("Egg Sandwich", true, false, false, true, false, false);
-                    EGG_SANDWICH.DisplayName();
-                    price = 5;
-                } else if (randomNum == 15) {
-                    order EGG_HAM_SANDWICH = new order("Egg and Ham Sandwich", true, false, false, true, true, false);
-                    EGG_HAM_SANDWICH.DisplayName();
-                    price = 5;
-                } else if (randomNum == 16) {
-                    order EGG_HAM_SANDWICH_LEMONADE = new order("Egg and Ham Sandwich Lemonade", true, true, false, true, true, false);
-                    EGG_HAM_SANDWICH_LEMONADE.DisplayName();
-                    price = 5;
-                } else if (randomNum == 17) {
-                    order EGG_OLIVE_SANDWICH = new order("Egg and Olive Sandwich", true, false, false, true, false, true);
-                    EGG_OLIVE_SANDWICH.DisplayName();
-                    price = 5;
-                } else if (randomNum == 18) {
-                    order EGG_OLIVE_SANDWICH_LEMONADE = new order("Egg and Olive Sandwich Lemonade", true, true, false, true, false, true);
-                    EGG_OLIVE_SANDWICH_LEMONADE.DisplayName();
-                    price = 5;
-                } else if (randomNum == 19) {
-                    order EGG_HAM_OLIVE_SANDWICH = new order("Egg and Ham and Olive Sandwich", true, false, false, true, true, true);
-                    EGG_HAM_OLIVE_SANDWICH.DisplayName();
-                    price = 5;
-                } else if (randomNum == 20) {
-                    order EGG_HAM_OLIVE_SANDWICH_LEMONADE = new order("Egg and Ham and Olive Sandwich Lemonade", true, true, false, true, true, true);
-                    EGG_HAM_OLIVE_SANDWICH_LEMONADE.DisplayName();
-                    price = 5;
-                }
+                genOrders();
                 //gameObject.FindChildByName("chocoCookie").setActive;
             }
         } 
@@ -291,6 +213,134 @@ public class orders : MonoBehaviour
         moving = true;
         cloned = false;
         
+    }
+    void genOrders()
+    {
+        randomNum = Random.Range(1, 20);
+
+        if (randomNum == 1)
+        {
+            order COOKIE = new order("Cookie", false, false, true, false, false, false);
+            COOKIE.DisplayName();
+            price = 5;
+            currentOrderPhoto.sprite = orderPhotos[0];
+        }
+        else if (randomNum == 2)
+        {
+            order COOKIE_LEMONADE = new order("Cookie Lemonade", false, true, true, false, false, false);
+            COOKIE_LEMONADE.DisplayName();
+            price = 5;
+            currentOrderPhoto.sprite = orderPhotos[1];
+        }
+        else if (randomNum == 3)
+        {
+            order BREAD = new order("Bread", false, false, false, true, false, false);
+            BREAD.DisplayName();
+            price = 5;
+            currentOrderPhoto.sprite = orderPhotos[2];
+        }
+        else if (randomNum == 4)
+        {
+            order BREAD_LEMONADE = new order("Bread Lemonade", false, false, true, true, false, false);
+            BREAD_LEMONADE.DisplayName();
+            price = 5;
+
+        }
+        else if (randomNum == 5)
+        {
+            order BREAD_HAM = new order("Bread and Ham", false, false, false, true, true, false);
+            BREAD_HAM.DisplayName();
+            price = 5;
+        }
+        else if (randomNum == 6)
+        {
+            order BREAD_HAM_LEMONADE = new order("Bread and Ham Lemonade", false, true, false, true, true, false);
+            BREAD_HAM_LEMONADE.DisplayName();
+        }
+        else if (randomNum == 7)
+        {
+            order BREAD_OLIVE = new order("Bread and Olive", false, false, false, true, false, true);
+            BREAD_OLIVE.DisplayName();
+            price = 5;
+        }
+        else if (randomNum == 8)
+        {
+            order BREAD_HAM_OLIVE = new order("Bread and Ham and Olive", false, false, false, true, true, true);
+            BREAD_HAM_OLIVE.DisplayName();
+            price = 5;
+        }
+        else if (randomNum == 9)
+        {
+            order BREAD_HAM_OLIVE_LEMONADE = new order("Bread and Ham and Olive Lemonade", false, true, false, true, true, true);
+            BREAD_HAM_OLIVE_LEMONADE.DisplayName();
+            price = 5;
+        }
+        else if (randomNum == 10)
+        {
+            order EGG = new order("Egg", true, false, false, false, false, false);
+            EGG.DisplayName();
+            price = 5;
+        }
+        else if (randomNum == 11)
+        {
+            order EGG_LEMONADE = new order("Egg Lemonade", true, true, false, false, false, false);
+            EGG_LEMONADE.DisplayName();
+            price = 5;
+        }
+        else if (randomNum == 12)
+        {
+            order EGG_OLIVE = new order("Egg and Olive", true, false, false, false, false, true);
+            EGG_OLIVE.DisplayName();
+            price = 5;
+        }
+        else if (randomNum == 13)
+        {
+            order EGG_OLIVE_LEMONADE = new order("Egg and Olive Lemonade", true, true, false, false, false, true);
+            EGG_OLIVE_LEMONADE.DisplayName();
+            price = 5;
+        }
+        else if (randomNum == 14)
+        {
+            order EGG_SANDWICH = new order("Egg Sandwich", true, false, false, true, false, false);
+            EGG_SANDWICH.DisplayName();
+            price = 5;
+        }
+        else if (randomNum == 15)
+        {
+            order EGG_HAM_SANDWICH = new order("Egg and Ham Sandwich", true, false, false, true, true, false);
+            EGG_HAM_SANDWICH.DisplayName();
+            price = 5;
+        }
+        else if (randomNum == 16)
+        {
+            order EGG_HAM_SANDWICH_LEMONADE = new order("Egg and Ham Sandwich Lemonade", true, true, false, true, true, false);
+            EGG_HAM_SANDWICH_LEMONADE.DisplayName();
+            price = 5;
+        }
+        else if (randomNum == 17)
+        {
+            order EGG_OLIVE_SANDWICH = new order("Egg and Olive Sandwich", true, false, false, true, false, true);
+            EGG_OLIVE_SANDWICH.DisplayName();
+            price = 5;
+        }
+        else if (randomNum == 18)
+        {
+            order EGG_OLIVE_SANDWICH_LEMONADE = new order("Egg and Olive Sandwich Lemonade", true, true, false, true, false, true);
+            EGG_OLIVE_SANDWICH_LEMONADE.DisplayName();
+            price = 5;
+        }
+        else if (randomNum == 19)
+        {
+            order EGG_HAM_OLIVE_SANDWICH = new order("Egg and Ham and Olive Sandwich", true, false, false, true, true, true);
+            EGG_HAM_OLIVE_SANDWICH.DisplayName();
+            price = 5;
+        }
+        else if (randomNum == 20)
+        {
+            order EGG_HAM_OLIVE_SANDWICH_LEMONADE = new order("Egg and Ham and Olive Sandwich Lemonade", true, true, false, true, true, true);
+            EGG_HAM_OLIVE_SANDWICH_LEMONADE.DisplayName();
+            price = 5;
+        }
     }
 }
 
